d3bda14bf62f3573892d4686110cbf0437fa2ef9
Vinstaal0 <vincent-staaltjens@live.nl>
2017-07-26 11:10:54 +0200

    Update HelloWorld.java

--- TextBasedRPG/HelloWorld.java
+++ TextBasedRPG/HelloWorld.java
@@ -5,13 +5,13 @@ public class HelloWorld {
     public static Scanner in = new Scanner(System.in);
     public static String input = "";
     
-    public static boolean continue = true;
+    public static boolean _continue = true;
 
     public static void main(String []args) {
         
         input = in.nextLine();
         
-        while(continue) {
+        while(_continue) {
             if (input.equalsIgnoreCase("q")) {
                 System.out.println("Shutting down system...");
             } else {
